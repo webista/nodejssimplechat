5c85c496fbd97442f8786b97b3fc3d88f7e19020
Barry Lavides <lavidesbarry@gmail.com>
2013-06-18 13:30:29 +0800

    - Polling is working already - Cannot add a new message yet

--- chat.js
+++ chat.js
@@ -6,9 +6,43 @@ var messages = ['testing'];
 var clients = [];
 
 http.createServer(function(req, res) {
-	fs.readFile('./index.html', function(err, data) {
-		res.end(data);
-	});
+	// Parse URL
+	var urlParts = url.parse(req.url);
+	console.log(urlParts);
+
+	if ( urlParts.pathname === '/' ) {
+		// File serving
+		fs.readFile('./index.html', function(err, data) {
+			res.end(data);
+		});
+	} else if ( urlParts.pathname.substring(0,5) === '/poll') {
+		// Polling code here
+		var count = urlParts.pathname.replace(/[^0-9]*/, '');
+		console.log(count);
+
+		if ( messages.length > count ) {
+			res.end(JSON.stringify({
+				count: messages.length,
+				append: messages.slice(count).join('\n')+'\n'
+			}));
+		} else {
+			clients.push(res);
+		}
+	} else if ( urlParts.pathname.substring(0,5) === '/msg/' ) {
+		// Message receiving
+		var msg = unescape(urlParts.pathname.substr(5));
+		messages.push(msg);
+
+		while(clients.length > 0) {
+			var client = clients.pop();
+			client.end(JSON.stringify({
+				count: messages.length,
+				append: msg+'\n'
+			}));
+		}
+
+		res.end();
+	}
 }).listen(8080, 'localhost');
 
 console.log('Server is running');
